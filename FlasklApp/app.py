3ebf98695e379880e04ee315e89b8fbab1e7898b
anasigbaria <44587049+anasigbaria@users.noreply.github.com>
2021-01-26 15:20:53 +0200

    Update app.py

--- FlasklApp/app.py
+++ FlasklApp/app.py
@@ -1,18 +1,42 @@
 from flask import Flask, render_template
 import requests, json
-import subprocess
-
+from flask import request
 
 
 app = Flask(__name__)
 
+def conv(frm=1,to=1,amount=1):
+    x = requests.get("http://172.28.0.1:6666/convert?cfrom="+str(frm)+"&cto="+str(to)+"&amount="+str(amount))
+    return x.text
+
+
 @app.route('/')
 def index():
-    cfrom = subprocess.check_output(['curl', 'http://127.0.0.1:8087/list'],shell=True)
-    cto = ['INS', 'EUR', 'DLR']
-    print(cto)
-    return render_template('index.html', cfrom=cfrom,cto=cto)
+    x = requests.get("http://172.28.0.1:6666/list")
+    lst= x.text
+    lst= lst[17:-2].replace('[',"").replace(']',"").replace("'" , "").replace(" ","").split(",")
 
+    return render_template('index.html', cfrom=lst,cto=lst)
+
+@app.route("/", methods=['POST'])
+def move_forward():
+    x = requests.get("http://172.28.0.1:6666/list")
+    lst= x.text
+    lst= lst[17:-2].replace('[',"").replace(']',"").replace("'" , "").replace(" ","").split(",")
+
+    z=(str(request.form['amount']))
+    if z:
+        z=int(z)
+    else :
+        z=0
+    
+    zz=1
+    if request.form['amount']== "":
+        zz=1
+    else:
+        zz=request.form['amount']
+    resl=conv(amount=zz,to=request.form['cto'],frm=request.form['cfrom'])
+    return render_template('index.html',res=resl, cfrom=lst,cto=lst)
 
 
 if __name__ == "__main__":
